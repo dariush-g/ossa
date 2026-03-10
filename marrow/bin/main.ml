21abc63462415bf4112691e43d15d5a6dfbd8311
dariush-g <dariush@ghassemieh.com>
2026-03-10 00:09:44 -0700

    work on build system

--- marrow/bin/main.ml
+++ marrow/bin/main.ml
@@ -1,1 +1,12 @@
-let () = print_endline "Hello, World!"
+let () =
+  if Array.length Sys.argv < 2 then (
+    prerr_endline "usage: marrow ...";
+    exit 1);
+
+  match Sys.argv.(1) with
+  | "init" -> ()
+  | "build" -> ()
+  | "run" -> ()
+  | arg ->
+      Printf.printf "unknown argument %s" arg;
+      exit 1
